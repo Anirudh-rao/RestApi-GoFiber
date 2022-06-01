e4c4e498630ae97d1ab89edc12a7058281f1b412
Anirudh Rao <anirudh131299@gmail.com>
2022-06-01 22:13:28 +0530

    Project Done

--- controllers/TodoController.go
+++ controllers/TodoController.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"github.com/Anirudh-rao/RESTAPI-GOFIBER/models"
+	"github.com/Anirudh-rao/RestApi-GoFiber/models"
 	"github.com/Kamva/mgm/v2"
 	"github.com/gofiber/fiber"
 	"go.mongodb.org/mongo-driver/bson"
@@ -40,6 +40,103 @@ func GetTodoByID(ctx *fiber.Ctx) {
 		ctx.Status(404).JSON(fiber.Map{
 			"ok":    false,
 			"error": "Todo not found.",
+		})
+		return
+	}
+
+	ctx.JSON(fiber.Map{
+		"ok":   true,
+		"todo": todo,
+	})
+}
+
+// CreateTodo - POST /api/todos
+func CreateTodo(ctx *fiber.Ctx) {
+	params := new(struct {
+		Title       string
+		Description string
+	})
+
+	ctx.BodyParser(&params)
+
+	if len(params.Title) == 0 || len(params.Description) == 0 {
+		ctx.Status(400).JSON(fiber.Map{
+			"ok":    false,
+			"error": "Title or description not specified.",
+		})
+		return
+	}
+
+	todo := models.CreateTodo(params.Title, params.Description)
+	err := mgm.Coll(todo).Create(todo)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(fiber.Map{
+		"ok":   true,
+		"todo": todo,
+	})
+}
+
+// ToggleTodoStatus - PATCH /api/todos/:id
+func ToggleTodoStatus(ctx *fiber.Ctx) {
+	id := ctx.Params("id")
+
+	todo := &models.Todo{}
+	collection := mgm.Coll(todo)
+
+	err := collection.FindByID(id, todo)
+	if err != nil {
+		ctx.Status(404).JSON(fiber.Map{
+			"ok":    false,
+			"error": "Todo not found.",
+		})
+		return
+	}
+
+	todo.Done = !todo.Done
+
+	err = collection.Update(todo)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(fiber.Map{
+		"ok":   true,
+		"todo": todo,
+	})
+}
+
+// DeleteTodo - DELETE /api/todos/:id
+func DeleteTodo(ctx *fiber.Ctx) {
+	id := ctx.Params("id")
+
+	todo := &models.Todo{}
+	collection := mgm.Coll(todo)
+
+	err := collection.FindByID(id, todo)
+	if err != nil {
+		ctx.Status(404).JSON(fiber.Map{
+			"ok":    false,
+			"error": "Todo not found.",
+		})
+		return
+	}
+
+	err = collection.Delete(todo)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"ok":    false,
+			"error": err.Error(),
 		})
 		return
 	}
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/Anirudh-rao/RestApi-GoFiber
 go 1.18
 
 require (
-	github.com/Kamva/mgm/v2 v2.0.0 // indirect
+	github.com/Kamva/mgm/v2 v2.0.0
+	github.com/gofiber/fiber v1.14.6
+	github.com/jozsefsallai/fiber-todo-demo v0.0.0-20200407163103-26dce515c9dd
+	go.mongodb.org/mongo-driver v1.9.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/gofiber/fiber v1.14.6 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
@@ -24,14 +29,10 @@ require (
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
-	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
-	github.com/xdg/stringprep v1.0.0 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	golang.org/x/text v0.3.5 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
